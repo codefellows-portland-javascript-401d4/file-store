5aaa5ac39f8f2058ca335668bbae542c71de1654
Christopher Bruner <chrbruner9@gmail.com>
2016-10-19 13:53:55 -0700

    initiated all files needed for testing and development, able to look at files in cats for store.js

---
+++ test.js
@@ -0,0 +1,12 @@
+var assert = require('chai').assert;
+var fs = require('fs');
+var store = require('./lib/store');
+
+describe('store', () => {
+    it('just checks whether we can call the api', done => {
+        console.log(fs.readdir('./cats', (err, files) => {
+            console.log(files);
+        }));
+        done();
+    });
+});
