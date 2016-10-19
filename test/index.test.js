dc8bb906f45b0a327a938d0885923708c8296109
Gloria Guy <totallygloria@gmail.com>
2016-10-19 16:12:19 -0700

    Project can now write a file to a folder using an object property as a name. Mocha tests wipe the directory and re-create it at the start of every test because it looks like Node doesn't have native support for deleting files and needs to use a helper function instead. Passes first functionality test!

--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,25 @@
 const assert = require('assert');
 const fs = require('fs');
+const path = require('path');
 const writeToFile = require('../index');
 
 
 describe('index', function() {
 
   beforeEach(function() {
-    var path = 'data';
+    var filepath = path.join('data', '');
     // remove the directory, because node can't delete files itself without a helper
-    fs.rmdir(path, function() {
+    fs.rmdir(filepath, function() {
       // make the directory again, so the files can go somewhere
-      fs.mkdir(path, function() {
+      fs.mkdir(filepath, function() {
         console.log('i removed and added the same dir!');
       })
     });
   });
 
-
   it('should name a file with the passed objs city property', function(done) {
 
     const testObj = {'City': 'Portland', 'Data': 'Hello World'};
-
     const callback = function() {
       const dir = 'data';  // this is the test data folder
 
@@ -36,4 +35,5 @@ describe('index', function() {
     };
     writeToFile(testObj, callback);
   })
-});
+
+}); // close describe
