120ebb68e93866990f23b59c7045fa404ab83b04
Nathan Hugon <nthugon@gmail.com>
2016-10-19 15:34:20 -0700

    setup project

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const store = require('../lib/store');
+const assert = require('assert');
+const elements = [
+  {
+    name: "hydrogen",
+    number: 1,
+    mass: 1,
+    symbol: "H"
+  },
+  {
+    name: "helium",
+    number: 2,
+    mass: 4,
+    symbol: "He"
+  },
+  {
+    name: "lithium",
+    number: 3,
+    mass: 7,
+    symbol: "Li"
+  }
+];
+store.saveFile(elements, 'elements');
