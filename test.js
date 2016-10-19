9caecbf3e9499e9d782ed026789cf417f0c68157
Christopher Bruner <chrbruner9@gmail.com>
2016-10-19 15:38:51 -0700

    completed storeObject function in store.js added more tests, storeObject will return the name of the file if the id already exists, e.g. the name

--- test.js
+++ test.js
@@ -1,14 +1,23 @@
 var assert = require('chai').assert;
 var fs = require('fs');
 var store = require('./lib/store');
+var rimraf = require('rimraf');
 
 describe('store', () => {
     it('just checks whether we can call the api', done => {
-        fs.readdir('./cats', (err, files) => { console.log(files);});
-        done();
+        fs.readdir('./cats', (err, files) => { console.log(files);
+            done();
+        });
     });
-    it('gets the foo', done => {
-        fs.readFile('./cats/foo.json', "utf-8", (err, data) => { console.log(data);});
-        done();
+    // it('gets the foo', done => {
+    //     fs.readFile('./cats/foo.json', 'utf-8', (err, data) => { console.log(data);
+    //         done();
+    //     });
+    // });
+    it('makes sure that storeObject is able to filter correctly', done => {
+        store.store({id: 'foo'}, './cats', done);
+    });
+    it('puts in a new object', done => {
+        store.store({id: 'felix'}, './cats', done);
     });
 });
