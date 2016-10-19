1cb2a856cd6f3d5e807223bd1e092ac1c4c97b64
Gloria Guy <totallygloria@gmail.com>
2016-10-19 16:38:04 -0700

    Node's built-in rmdir doesn't have the -rf recursive force functionality, and there's nothing equivalent built-in without recursing yourself. Installed third-party module rmrf to handle wiping the directory before each test, to avoid test collisions. Saving progress before testing it out.

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,20 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
+const rmrf = require('rmrf');
 const writeToFile = require('../index');
 
 
 describe('index', function() {
 
-  beforeEach(function() {
-    var filepath = path.join('data', '');
-    // remove the directory, because node can't delete files itself without a helper
-    fs.rmdir(filepath, function() {
-      // make the directory again, so the files can go somewhere
-      fs.mkdir(filepath, function() {
-        console.log('i removed and added the same dir!');
-      })
+  beforeEach(function(done) {
+    rmrf('data/', function(err) {
+      if (err) {
+        done(err)
+      } else {
+        console.log('i removed the dir!');
+        done();
+      }
     });
   });
 
@@ -41,11 +42,11 @@ describe('index', function() {
     const callback = function() {
       const filepath = 'data/Berkeley.txt';  // this is the test data folder
 
-      fs.readFile(filepath, function (err, data) {
+      fs.readFile(filepath, 'utf8', function (err, data) {
         if (err) {    // if you get an error, pass it to the callback to handle
           done(err);
         } else {
-          assert.equal(data, "{'City': 'Berkeley', 'Data': 'Hello World'}");
+          assert.equal(data, '{"City":"Berkeley","Data":"Hello World"}');
           done();
         }
       })
