2a403ab56d795813be70165a813ca3f3ee2d422d
David Chase <dchasepdx@gmail.com>
2016-10-19 16:22:04 -0700

    Wrote writeFile function and wrote test for writeFile function.

--- dotaTeam-test.js
+++ dotaTeam-test.js
@@ -1,23 +1,33 @@
 var assert = require('assert');
 var rimraf = require('rimraf');
 var mkdirp = require('mkdirp');
-var dotaTeamToJSON = require('./dotaTeam');
+const fs = require('fs');
+var fileStore = require('./dotaTeam');
 
 describe('File writer', function() {
-
-  it('Should take object and convert into JSON format', function() {
-    var evilGeniuses = {
-      teamMembers: ['Arteezy', 'Sumail' ,'Universe', 'Zai', 'Cr1t', ],
+      var newTeam = {
+      teamName: 'Evil Geniuses',
+      teamMembers: ['Arteezy', 'Sumail' ,'Universe', 'Zai', 'Cr1t'],
       region: 'NA',
       tiWinner: true
     }
-    assert(dotaTeamToJSON(evilGeniuses), {"teamMembers":["Arteezy","Sumail","Universe","Zai","Cr1t"],"region":"NA","tiWinner":true});  
+
+  it('Should take object and convert into JSON format', function() {
+
+    assert(fileStore.dotaTeamToJSON(newTeam), {"teamName":"Evil Geniuses","teamMembers":["Arteezy","Sumail","Universe","Zai","Cr1t"],"region":"NA","tiWinner":true});  
   });
 
-  it('Should store new object in new file with identifier for file name', function() {
-
+  it('Should store new object in new file with identifier for file name', function(done){
+    fileStore.createFile(newTeam, function(err) {
+        if (err) return done(err);
+        fs.readFile('./dotaTeams/EvilGeniuses.json', 'utf8', (err, data) => {
+          //if (err) return done(err);
+          // console.log('here');
+          assert.equal(data, '{"teamName":"Evil Geniuses","teamMembers":["Arteezy","Sumail","Universe","Zai","Cr1t"],"region":"NA","tiWinner":true}');
+          done();
+      });
+    });
   });
-
 });
 
 describe('File retriever', function() {
--- dotaTeam.js
+++ dotaTeam.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
-var dotaTeamToJSON = function(teamObj) {
+const path = require('path');
+var fileStore = {};
+
+fileStore.dotaTeamToJSON = function(teamObj) {
   return JSON.stringify(teamObj);
 }
-module.exports = dotaTeamToJSON;
+
+fileStore.createFile = function(teamObj, callback) {
+  var teamPath = teamObj.teamName.split(' ').join('');
+  var filePath = path.join('./dotaTeams', teamPath);
+  filePath += '.json';
+  var teamJSON = fileStore.dotaTeamToJSON(teamObj);
+  fs.writeFile(filePath, teamJSON, 'utf8', function(err) {
+    if (err) return callback(err);
+    callback();
+  });
+}
+
+
+module.exports = fileStore;
+
