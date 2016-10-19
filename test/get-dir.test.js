2df127e02573d89c92e776aeb834b6b1dfc9ba41
elhendricks <elhendricks@gmail.com>
2016-10-19 16:48:22 -0700

    index.txt test working, but not passing

---
+++ test/get-dir.test.js
@@ -0,0 +1,10 @@
+var getDir = require('../lib/get-dir');
+var assert = require('assert');
+var fs = require('fs');
+
+describe('get-dir', function() {
+    it('gets all file contents', function(done) {
+
+        done();
+    })
+})
---
+++ test/index-dir.test.js
@@ -0,0 +1,34 @@
+var indexDir = require('../lib/index-dir');
+var assert = require('assert');
+var fs = require('fs');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+
+
+
+describe('index-dir', function(){
+    var tree1 = {name: "oak", deciduous: false, age: 150}
+    var tree2 = {name: "pine", deciduous: false, age: 20}
+    var tree3 = {name: "maple", deciduous: true, age: 50}
+    var dirPath = 'tree-dir/';
+    
+    // before(function() {
+    //     rimraf(dirPath + 'index.txt', console.log('Error: 'err));
+    //     rimraf(dirPath + '*.json', console.log('Error: 'err));
+    // })
+
+    
+    it('writes directory list', function(done){
+        var callback = function(err, data) {
+            console.log(__dirname);
+            if (err) return done(err);
+            var index = fs.readFileSync(dirPath + 'index.txt', 'utf-8');
+            assert.equal(index, 'oak.txt\npine.txt\nmaple.txt');
+            
+            done();
+        }
+
+        indexDir(dirPath, callback);
+          
+    })
+})
