1f52150eabba8ee12b2025f0f6b3e7de0daa64bb
Andrew Beers <andybeers@gmail.com>
2016-10-19 13:41:02 -0700

    Added basic test framework

---
+++ dotaTeam-test.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+var dotaTeam = require('./dotaTeam');
+
+describe('File writer', function() {
+
+  it('Should take object and convert into JSON format', function() {
+
+  });
+
+  it('Should store new object in new file with identifier for file name', function() {
+
+  });
+
+});
+
+describe('File retriever', function() {
+
+  it('Should retrieve individual resource by identifier', function() {
+
+  });
+
+  it('Should retrieve list of all resources, ordered by identifier', function() {
+
+  });
+
+});
