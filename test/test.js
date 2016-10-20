28efd1ada303539487ecb1bfb1adf2cc40bea966
Nathan Hugon <nthugon@gmail.com>
2016-10-20 15:35:54 -0700

    all tests are passing

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const store = require('../lib/store.js');
 const assert = require('assert');
 const fs = require('fs');
 const get = require('../lib/get.js');
+const rimraf = require('rimraf');
 
 const elements = [
   {
@@ -23,47 +24,79 @@ const elements = [
     symbol: "Li"
   },
   {
-    name: "Beryllium",
+    name: "beryllium",
     number: 4,
     mass: 9,
     symbol: "Be"
   },
   {
-    name: "Boron",
+    name: "boron",
     number: 5,
     mass: 11,
     symbol: "B"
   }
 ];
 
-// describe('file system', function() {
+describe('File Store System', function() {
+
+  before(function(done) {
+    rimraf('../data/elements', done);
+    console.log('Rimraffed!');
+  });
 
 
-//   it('elements folder exists', function(done){
-//     function JSONtest(err) {
-//       if (err) return done(err);
-//       console.log('Stinky Farts!');
-//       done();
-//     };
-//     get.mergeAllFiles('elements', callback);
-//   });
+  it('elements folder exists', function(done) {
+    // test function
+    function directoryTest(err) {
+      if (err) return done(err);
+      assert.ok(fs.existsSync('../data/elements'));
+      done();
+    };
+    // run save elements with test function
+    store.saveFile(elements, 'elements', directoryTest);
+  });
+
+  it('creates JSON files in the directory whose contents are equal to the original content when parsed', function(done){
+    function secondTest(arr) {
+      console.log('test 2 running');
+      assert.deepEqual(elements, arr);
+      done();
+    };  
+    get.mergeAllFiles('elements', secondTest);  
+  });
+
+  it('returns requested files', function(done){
+    //test function
+    function reqEleTest(arr) {
+      assert.deepEqual(arr, [{ name: "helium",number: 2,mass: 4,symbol: "He"}, { name: "lithium",number: 3,mass: 7,symbol: "Li"}, { name: "beryllium", number: 4, mass: 9, symbol: "Be"}]);
+      done();
+    };
+    get.findMultipleFiles(['beryllium', 'lithium', 'helium'], reqEleTest);
+
+  //   console.log('Variable reqEle:', reqEle);
+  //   assert.equal(reqEle, '[{name: "Beryllium", number: 4, mass: 9, symbol: "Be"},{name: "lithium",number: 3,mass: 7,symbol: "Li"},{name: "helium",number: 2,mass: 4,symbol: "He"}]');
+  });
+
+  it('creates a JSON file for a single specified element', function(done){
+    function fourthTest(arr) {
+      console.log('test 4 running');
+      assert.deepEqual([{name: "beryllium", number: 4, mass: 9,symbol: "Be"}], arr);
+      done();
+    };  
+    get.findMultipleFiles(['beryllium'], fourthTest);  
+  });
 
 
-//   it('creates correct number of files', function(){
+});
 
-//   });
+// function testCallback() {
+//   console.log('Test callback!');
+// };
+// function callMergeAll(){
+//   get.mergeAllFiles('elements', testCallback);
+// };
 
-// });
-
-function testCallback() {
-  console.log('Test callback!');
-};
-function callMergeAll(){
-  get.mergeAllFiles('elements', testCallback);
-};
-
-store.saveFile(elements, 'elements', callMergeAll);
-
+// store.saveFile(elements, 'elements', callMergeAll);
 
 
 
