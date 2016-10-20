15a2d0f6db623c06260c37df5acc423cab35e446
Waxhoya <waxhoya@gmail.com>
2016-10-19 17:30:53 -0700

    pushing up for the day

--- test.js
+++ test.js
@@ -3,36 +3,29 @@ var fs = require('fs');
 var store = require('./lib/store');
 var rimraf = require('rimraf');
 
+// test case cat removal
+rimraf('./cats/felix.json', function(err){
+    console.log(err);
+});
+
 describe('store', () => {
     it('just checks whether we can call the api', done => {
         fs.readdir('./cats', (err, files) => { console.log(files);
             done();
         });
     });
-    // it('gets the foo', done => {
-    //     fs.readFile('./cats/foo.json', 'utf-8', (err, data) => { console.log(data);
-    //         done();
-    //     });
-    // });
     it('makes sure that storeObject is able to filter correctly', done => {
         store.store({id: 'foo'}, './cats', done);
     });
     it('puts in a new object', done => {
-        store.store({id: 'felix'}, './cats', done);
+        store.store({id: 'felix'}, './cats', function(err, data){
+            assert.equal(data, 'felix');
+            done()
+        });
     });
     it('retrieves object by id', done => {
         store.retrieve('felix', './cats', done);
     });
-    // it('checking if I can hard code read the file', done => {
-    //     fs.readFile('./cats/felix.json', 'utf-8', (err, data) => {
-    //         if (err) console.log(err);
-    //         console.log('data');
-    //         done();
-    //     });
-    // });
+    it('Deep comparison of objects')
 });
 
-// test case cat removal
-rimraf('./cats/felix.json', function(err){
-    console.log(err);
-});
