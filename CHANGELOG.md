a4dcc880150af7d55f73d53e3c79c50173185776
Gloria Guy <totallygloria@gmail.com>
2016-10-19 22:54:57 -0700

    I was checking for arrays using a for in loop, which in a large enough hash will hive the keys back randomly. Discovered the .constructor method to check the difference between an array and an object instead. Cleaned up linter errors as well.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ## [Unreleased]
 ### Added
 
+## [1.0.1] - 2016-10-18
+### Changed
+- Updated the implementation of the type checking to be more accurate in distinguishing arrays from other objects.
+- Fixed linter errors.
+
 ## [1.0.0] - 2016-10-18
 ### Added
 - API Changed! Incompatible with pre-release versions. You now *must* pass the identifier to save/search for in the call.
--- index.js
+++ index.js
@@ -6,33 +6,38 @@ function store(data, identifier, cb) {
   var filename, filepath;
 
   switch (typeof data) {
-    case 'number':
-      filename = data.toString() + '.json';
-      filepath = path.join('data', filename);
-      break;
-    case 'string':
-      if (data.length > 255) {
-        data = data.slice(0,50);    // arbitrary slicing
+  case 'number':
+    filename = data.toString() + '.json';
+    filepath = path.join('data', filename);
+    break;
+  case 'string':
+    if (data.length > 255) {
+      data = data.slice(0,50);    // arbitrary slicing
+    }
+    filename = data.replace(/ /g,'_') + '.json';
+    filepath = path.join('data', filename);
+    break;
+  case 'object':
+    if (data.constructor == Array) {   // If identifier valid, use it, otherwise grab the first element
+      if (data[identifier]) {
+        filename = data[identifier].toString() + '.json';
+        filepath = path.join('data', filename);
+        break;
+      } else {
+        filename = data[0].toString() + '.json';
+        filepath = path.join('data', filename);
+        break;
       }
-      filename = data.replace(/ /g,'_') + '.json';
-      filepath = path.join('data', filename);
-      break;
-    case 'object':
-      for (var key in data) {
-        if (key == 0) {   // I'm an array! non strict, because the loop coerces numbers into strings
-          filename = data[0].toString() + '.json';
-          filepath = path.join('data', filename);
-          break;
-        } else if (data[identifier] && typeof data[identifier] != 'function') {    // If the identifier is in the object
-          filename = data[identifier].toString().replace(/ /g, '_') + '.json';
-          filepath = path.join('data', filename);
-          break;
-        } else {    // The identifier isn't valid, throw an error
-          cb('Error, unknown identifier or value is a function. Please select another identifier.');
-          break;
-        }
+    } else if (data.constructor == Object) {
+      if (data[identifier] && typeof data[identifier] != 'function') {    // If the identifier is in the object
+        filename = data[identifier].toString().replace(/ /g, '_') + '.json';
+        filepath = path.join('data', filename);
+        break;
+      } else {    // The identifier isn't valid, throw an error
+        cb('Error, identifier value is a function. Please select another identifier.');
+        break;
       }
-      break;
+    }
   } // end switch
 
   var stringData = JSON.stringify(data);
@@ -53,7 +58,7 @@ function get(filename, cb) {
   fs.readFile(filename, 'utf8', function (err, filedata) {
     if (err) {    // if you get an error, pass it to the callback to handle
       console.log('you got a read error: ', err);
-      cb(err)
+      cb(err);
     } else {    // pass the data back to whoever asked for it
       var jsondata = JSON.parse(filedata);
       cb(jsondata);
@@ -66,17 +71,17 @@ function getAll(dir, cb) {
   fs.readdir(dir, function (err, files) {
     if (err) {    // if you get an error, pass it to the callback to handle
       console.log('you got a real directory error: ', err);
-      cb(err)
+      cb(err);
     } else {    // pass the data back to whoever asked for it
       cb(files);
     }
   });
 }
 
-// function callback(response) {
-//   console.log('callback passed: ', response);
-// }
-// store({'a':12,'b':234},'a', callback);
+function callback(response) {
+  console.log('callback passed: ', response);
+}
+store([1,2,3,4],'a', callback);
 
 module.exports.store = store;
 module.exports.get = get;
