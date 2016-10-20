7e30ffd813cddf166e34affc153dccfa2b8c5be7
Gloria Guy <totallygloria@gmail.com>
2016-10-19 20:09:56 -0700

    Added a get method to try to grab a resource by name, and if it's there, pass the data back to the caller. Works in cli, starting to write tests next.

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ function store(data, cb) {
   });
 }
 
+function get(filename, cb) {
+
+  fs.readFile(filename, 'utf8', function (err, filedata) {
+    if (err) {    // if you get an error, pass it to the callback to handle
+      cb(err)
+    } else {    // pass the data back to whoever asked for it
+      var jsondata = JSON.parse(filedata);
+      cb(filedata);
+    }
+  });
+}
+
 function getAll(dir, cb) {
 
   fs.readdir(dir, function (err, files) {
@@ -30,27 +42,15 @@ function getAll(dir, cb) {
   });
 }
 
-function get(dir, cb) {
-
-  fs.readdir(dir, function (err, files) {
-    if (err) {    // if you get an error, pass it to the callback to handle
-      cb(err)
-    } else {    // pass the data back to whoever asked for it
-      console.log(files);
-      cb(files);
-    }
-  });
-}
-
-function ohhai() {
-  console.log('im a callback');
+function ohhai(results) {
+  console.log('callback called with: ', results);
 }
 //
 // var city = {'City':'New York','State':'NY','Median_1_BR_price':'$3,370','Median_2_BR_price':'$4,820'};
 // store(city, ohhai);
 
-// getAll('/data', ohhai);
+get('data/Boston.txt', ohhai);
 
 module.exports.store = store;
-module.exports.getAll = getAll;
 module.exports.get = get;
+module.exports.getAll = getAll;
